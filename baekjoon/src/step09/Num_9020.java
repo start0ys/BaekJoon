ff5340e10bb973c25f17be1c4f66f43073128d0d
시작 <78341551+start0ys@users.noreply.github.com>
2021-12-06 23:38:35 +0900

    9단계 - 기본 수학2 (진행중)

---
+++ baekjoon/src/step09/Num_9020.java
@@ -0,0 +1,40 @@
+package step09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num_9020 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		int t = Integer.parseInt(br.readLine());
+		int num;
+		boolean [] primeNum = new boolean[10001];
+		primeNum[0] = primeNum[1] = true;
+		for(int i =2; i<100; i++) {
+			if(!primeNum[i]) {
+				for(int j = i+i; j<10000; j+=i) {
+					primeNum[j]=true;
+				}
+			}
+		}
+		while(t-->0) {
+			num = Integer.parseInt(br.readLine());
+			int a = num/2;
+			int b = num/2;
+			while(true) {
+				if(!primeNum[a] && !primeNum[b]) {
+					sb.append(b+" "+a).append("\n");
+					break;
+				}
+				a+=1;
+				b-=1;
+			}
+		}
+		System.out.println(sb);
+
+	}
+
+}
