fe9c5e5a476956f04c28abd4636e97b60a88a450
시작 <78341551+start0ys@users.noreply.github.com>
2021-12-30 22:54:26 +0900

    14단계 - 백트래킹 (진행중)

---
+++ baekjoon/src/step14/Num_15652.java
@@ -0,0 +1,40 @@
+package step14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Num_15652 {
+	public static int [] arr;
+	public static StringBuffer sb = new StringBuffer();
+	public static int n,m;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
+		dfs(0,0);
+		System.out.println(sb);
+
+	}
+	
+	public static void dfs(int depth,int idx) {
+		if(m == depth) {
+			for(int result : arr) {
+				sb.append(result + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = idx; i < n; i++) {
+			arr[depth] = i + 1;
+			dfs(depth+1,i);
+				
+		}
+		return;
+	}
+
+}
